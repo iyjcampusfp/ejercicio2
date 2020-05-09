b9a69569bd349f5700830a238f0aeb3b519c7e3a
iyjcampusfp <iyjcampusfp@gmail.com>
2020-05-09 19:30:59 +0200

    Atributos clase Persona

--- src/metodos/Persona.java
+++ src/metodos/Persona.java
@@ -2,4 +2,12 @@ package metodos;
 
 public class Persona {
     
+    private String nombre;
+    private int edad;
+    private String DNI;
+    private char sexo;
+    private double peso;
+    private double altura;
+    
+    
 }
