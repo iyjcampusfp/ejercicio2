c51ffa6597a9d8c4c94c60944140d2a976b92e71
IyJ <iyjcampusfp@gmail.com>
2020-05-09 19:36:13 +0200

    Comentarios y Estructuras

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         
+        
+        
+        
+        
+        //
+        
+        
+        
+        
+        
+        
     }
     
 }
--- src/metodos/Persona.java
+++ src/metodos/Persona.java
@@ -1,13 +1,35 @@
 package metodos;
 
 public class Persona {
-    
+    /**************************************************************************************/
+                                     //Atributos
+    //Atributos de la Clase Persona
     private String nombre;
     private int edad;
-    private String DNI;
-    private char sexo;
+    private String DNI;//Será automático , ya que será dado por un método
+    private char sexo;//Será un char porque queremos saber si es H (hombre) o M (mujer)
     private double peso;
     private double altura;
+    /*************************************************************************************/
+                                    //Contructores
+    
+    
+    
+    
+    
+    /**************************************************************************************/
+                                    //Getters y Setters
+    //Getters
+    
+    
+    //Setters
+    
+    
+    /***************************************************************************************/
+                                    //Métodos Personales
+    
+    
+    
     
     
 }
